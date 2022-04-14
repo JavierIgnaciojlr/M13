cf14f9af302089066075fa0973af02bca3a90992
JavierIgnaciojlr <lopez.rivas.javier@gmail.com>
2022-04-14 18:34:10 -0400

    third commit

--- pokemones.sql
+++ pokemones.sql
@@ -13,7 +13,6 @@ CREATE TABLE pokemones(
 SELECT * FROM pokemones;
 
 \copy pokemones FROM 'pokemonesKanto.csv' csv header;
-SELECT * FROM pokemones LIMIT 10;
 
 CREATE TABLE mis_pokemones(
     pokedex INT,
@@ -24,32 +23,48 @@ CREATE TABLE mis_pokemones(
     estatura FLOAT,
     FOREIGN KEY (pokedex) REFERENCES pokemones(pokedex)
 );
-SELECT * FROM mis_pokemones;
+-- SELECT * FROM mis_pokemones;
 
 \copy mis_pokemones FROM 'mis_pokemones.csv' csv header;
--- SELECT * FROM mis_pokemones LIMIT 5;
 
 -- SELECT * FROM mis_pokemones;
 -- SELECT * FROM pokemones;
 
+-- ver que pokemones tengo
 -- SELECT * FROM pokemones
 -- JOIN mis_pokemones
 -- ON pokemones.pokedex = mis_pokemones.pokedex;
 
+--necesitamos saber que pokemones no tenemos
+-- como dice saber que pokemones no tenemos mostramos info de pokemones
 -- SELECT pokemones.*
 -- FROM pokemones
 -- LEFT JOIN mis_pokemones
 -- ON pokemones.pokedex = mis_pokemones.pokedex
 -- WHERE mis_pokemones.pokedex IS NULL;
 
--- SELECT *
+-- SELECT * 
 -- FROM mis_pokemones
 -- LEFT JOIN pokemones
--- ON mis_pokemones.pokedex = pokedex.pokedex;
+-- ON mis_pokemones.pokedex = pokemones.pokedex;
 
-SELECT *
-FROM mis_pokemones
-LEFT OUTER JOIN pokemones
-ON mis_pokemones.pokedex = pokedex.pokedex;
+-- SELECT *
+-- FROM mis_pokemones
+-- FULL OUTER JOIN pokemones
+-- ON mis_pokemones.pokedex = pokemones.pokedex;
 
+-- SELECT nombre
+-- FROM pokemones
+-- WHERE pokedex
+-- IN (SELECT pokedex
+-- FROM mis_pokemones);
 
+SELECT DISTINCT pokedex 
+FROM mis_pokemones;
+
+-- la instruccion IN compara con un rango [1,2,3,4,5,6,7]
+SELECT nombre
+FROM pokemones
+WHERE pokedex
+IN (SELECT DISTINCT pokedex 
+FROM mis_pokemones);
